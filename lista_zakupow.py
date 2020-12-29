880ffed92035a11dd8050fe85a7dfd321c317d5f
Michał Majchrzak <mmajchrzak1@gmail.com>
2020-12-29 22:50:40 +0100

    Add for loop

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,6 @@ shopping_list = {
     "piekarnia" : ['chleb', 'pączek', 'bułki'],
     "warzywniak" : ['marchew', 'seler', 'rukola']
 }
+
+for key, items in shopping_list.items():
+    print(f"Idę do {key} i kupuję tam {items}")
